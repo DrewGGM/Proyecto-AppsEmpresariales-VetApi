9b313e3eb9550d33e7bc4aaddc8851fed18fda76
Andrew Garcia <145233179+DrewGGM@users.noreply.github.com>
2025-03-16 10:03:55 -0500

    feat(project): implementación de clases base para auditoría y manejo de excepciones

---
+++ src/main/java/com/vetapi/infrastructure/persistence/entity/base/BaseJpaEntity.java
@@ -0,0 +1,33 @@
+package com.vetapi.infrastructure.persistence.entity.base;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+// Clase base para todas las entidades JPA con atributos comunes y configuración de auditoría
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private boolean active = true;
+}
